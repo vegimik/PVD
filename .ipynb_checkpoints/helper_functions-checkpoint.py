ff84cac41a1aa3bf38fc0a172c82db321412b3e9
vegimik <vegimi.k@hotmail.com>
2021-12-11 13:45:40 +0100

    3. Integration and Aggregation

--- .ipynb_checkpoints/helper_functions-checkpoint.py
+++ .ipynb_checkpoints/helper_functions-checkpoint.py
@@ -1,5 +1,10 @@
 import pandas as pd
-#leximi i datasetit ku kthehet leximi
-def read():
-    return pd.read_csv(r"C:\\Users\\Qendrim Demiraj\\Downloads\\autoscout24-germany-dataset.csv")
+
+#Ngarkimi i datasetit ku kthehet si e 'lexuar'
+def readDataset():
+    return pd.read_csv(r"autoscout24-germany-dataset.csv")
+
+#Ngarkimi i datasetit ku kthehet si e 'lexuar' permes emrit
+def readManufacturers():
+    return pd.read_csv(r"manufacturers.csv")
 
--- helper_functions.py
+++ helper_functions.py
@@ -4,3 +4,7 @@ import pandas as pd
 def readDataset():
     return pd.read_csv(r"autoscout24-germany-dataset.csv")
 
+#Ngarkimi i datasetit ku kthehet si e 'lexuar' permes emrit
+def readManufacturers():
+    return pd.read_csv(r"manufacturers.csv")
+
