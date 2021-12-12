3a7030012f703ea6df0892da47fdbba61e4649a6
Qendrim Demiraj <qendrim.demiraj@ucxtech.com>
2021-12-12 19:50:22 +0100

    Syntax check

--- _task4.py
+++ _task4.py
@@ -16,21 +16,22 @@ def reductionAndSubset():
 reductionAndSubset()
 
 #Uniform Manifold Approximation and Projection (UMAP)
-dataset = _task1.preProcessingOfData()
+def uniformedApproximationAndProjection():
+    dataset = _task1.preProcessingOfData()
 
-print("Data Reduction started for Main Data Set.")
-wanted_data_volume = ['mark', 'model','fuel','gear','offerType']
-print("Dimensionality reduction.")
-print(f"Wanted columns:\n {wanted_data_volume}")
-print("Data Reduction has been successfully reducted.\n")
-dataset = dataset[list(wanted_data_volume)]
+    print("Data Reduction started for Main Data Set.")
+    wanted_data_volume = ['mark', 'model','fuel','gear','offerType']
+    print("Dimensionality reduction.")
+    print(f"Wanted columns:\n {wanted_data_volume}")
+    print("Data Reduction has been successfully reducted.\n")
+    dataset = dataset[list(wanted_data_volume)]
 
-print("Subset Properties \nDropping same row vaules of new subset...")
-dataset = dataset.drop_duplicates(subset=wanted_data_volume,keep=False)
+    print("Subset Properties \nDropping same row vaules of new subset...")
+    dataset = dataset.drop_duplicates(subset=wanted_data_volume,keep=False)
 
-print("Showing up the subset...")
-dataset.rename(columns={'DisplayName': 'Subset'},inplace=True)
-print(dataset)
+    print("Showing up the subset...")
+    dataset.rename(columns={'DisplayName': 'Subset'},inplace=True)
+    print(dataset)
 
 filter = "COUNT > 500"
 field_properties = "Field1 FLOAT true;Field2 STRING true;Field3 DOUBLE true"
@@ -38,3 +39,28 @@ file_extenstion = "csv"
 has_header_row = True
 file_encoding = "UTF-8"
 
+def getOnlySomeColumnsFromDataSet():
+    data = hf.readDataset()
+    # generate gaussian data sample
+    wanted_data_volume = ['price','year']
+    data = data[list(wanted_data_volume)]
+    data = data.drop_duplicates(subset=wanted_data_volume,keep=False)
+    data.rename(columns={'DisplayName': 'Subset'},inplace=True)
+    return data
+
+def discretionOfDataset():
+    dataset = getOnlySomeColumnsFromDataSet()
+    # summarize the shape of the dataset
+    print(dataset.shape)
+    # summarize each variable
+    print(dataset.describe())
+    # histograms of the variables
+    dataset.hist()
+    pyplot.show()
+    
+    # perform a k-means discretization transform of the dataset
+    trans = KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='kmeans')
+    dataset = trans.fit_transform(dataset)
+    print(dataset)
+
+discretionOfDataset()
